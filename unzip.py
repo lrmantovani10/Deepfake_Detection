c6fe054be25e21a88f9a8c6351b25b78d5e29691
lrmantovani10 <lrmantovani10@gmail.com>
2023-06-25 20:39:08 -0500

    zipping

---
+++ unzip.py
@@ -0,0 +1,8 @@
+from zipfile import ZipFile
+file_path= "data_zip.zip"
+
+# Extract all the contents of zip file in current directory
+with ZipFile(file_path, 'r') as zip:
+    zip.printdir()
+    zip.extractall()
+    print('Done!')
